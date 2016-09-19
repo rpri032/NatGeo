d5ee35cbe243d3e2b34d1e967d462b4ad6e30734
Richard Priest <richardpriest@gmail.com>
2016-09-19 14:17:38 -0400

    Test Commit

--- NatGeo/NatGeoApp/views.py
+++ NatGeo/NatGeoApp/views.py
@@ -12,7 +12,7 @@ def searchView(request):
     address = AddressModel("1150 K Street", "Washington", "20005", "DC", "United States")
     test = AddressService.StoreAddress(address)
     context = {
-        "firstName": "Richard",
+        "firstName": "RichardTEst",
         "lastName": "Priest",
         "address": str(address)
     }
